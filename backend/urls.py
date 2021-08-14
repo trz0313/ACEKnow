21ffc83f5aa4c768c50a9ea6f5e9e5f565cdafad
Richard Tang <86671538+trz0313@users.noreply.github.com>
2021-08-14 09:46:56 +0800

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from django.conf import settings
 from .views import GetDebugInfoAPIView
+from backend.views import (
+    passage_detail_view,
+    passage_update_view,
+    passage_delete_view,
+    passage_create_view,
+    PassageListView,
+)
 
 # url patterns
 urlpatterns = [
     path('getDebugInfo/', GetDebugInfoAPIView.as_view(), name='get_debug_info'),
-]
+    path('<slug>/', passage_detail_view, name='detail'),
+    path('<slug>/update', passage_update_view, name='update'),
+    path('<slug>/delete', passage_delete_view, name='delete'),
+    path('create', passage_create_view, name="create"),
+    path('list', PassageListView.as_view(), name="list"),
